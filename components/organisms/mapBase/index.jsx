693a613e1876b773e022e88e44a1b84746d0ceb0
Lalaso <raraso2000@icloud.com>
2019-11-22 10:33:51 +0900

    地図表示のアクセストークンをCookieから持ってくるように変更

--- components/organisms/mapBase/index.jsx
+++ components/organisms/mapBase/index.jsx
@@ -19,6 +19,20 @@ class MapBase extends React.Component {
       [this.state.lat, this.state.lng],
       this.state.zoom
     );
+
+    // ユーザーデータ取得(cookieから持ってくる)
+    const userData = { user_id: "", access_token: "" };
+    const r = document.cookie.split(";");
+    r.forEach(function(value) {
+      const content = value.split("=");
+      content[0] = content[0].replace(" ", "");
+      if (content[0] == "user_id") {
+        userData.user_id = content[1];
+      } else if (content[0] == "access_token") {
+        userData.access_token = content[1];
+      }
+    });
+
     const mainLayer = L.tileLayer
       .wmsHeader(
         "https://pascali.info-mapping.com/webservices/publicservice/WebmapServiceToken.asmx/WMSService?TENANTID=210005",
@@ -34,7 +48,7 @@ class MapBase extends React.Component {
         [
           {
             header: "X-Map-Api-Access-Token",
-            value: "2e8f941d-442e-457c-8b7e-c9c4e6334a38"
+            value: userData.access_token
           }
         ]
       )
