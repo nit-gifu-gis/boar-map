cc56dc75c5e3eb68b0347e7bb489cc031ed6650f
lalaso2000 <raraso2000@icloud.com>
2019-12-03 14:11:04 +0900

    中心にピンが立つように変更（アニメーション付き） #13

--- components/organisms/mapForAddInfo/index.jsx
+++ components/organisms/mapForAddInfo/index.jsx
@@ -1,7 +1,6 @@
 import "./mapForAddInfo.scss";
 import React from "react";
 import L from "leaflet";
-import "leaflet-center-cross";
 import MapBase from "../mapBase";
 import AddInfoFooter from "../../molecules/addInfoFooter";
 import Router from "next/router";
@@ -12,9 +11,42 @@ class MapForAddInfo extends MapBase {
 
   map() {
     super.map();
-    // Todo: 一番上のレイヤーに持ってくる
-    const centerCross = L.centerCross();
-    this.myMap.addLayer(centerCross);
+    const mapObj = this.myMap;
+    // 十字
+    const centerCrossIcon = L.icon({
+      iconUrl: "../../static/images/map/centerCross.png",
+      iconRetinaUrl: "../../static/images/map/centerCross.png",
+      iconSize: [40, 20],
+      iconAnchor: [21, 11]
+    });
+    const centerCross = L.marker(mapObj.getCenter(), {
+      icon: centerCrossIcon,
+      zIndexOffset: 400
+    }).addTo(mapObj);
+    // ピン
+    const centerPinIcon = L.icon({
+      iconUrl: "../../static/images/map/centerPin.svg",
+      iconRetinaUrl: "../../static/images/map/centerPin.svg",
+      iconSize: [60, 120],
+      iconAnchor: [31, 84]
+    });
+    const centerPin = L.marker(mapObj.getCenter(), {
+      icon: centerPinIcon,
+      zIndexOffset: 400
+    }).addTo(mapObj);
+    // ピンを動かしている間、ピンを少し浮かせる
+    mapObj.on("move", function(e) {
+      const point = mapObj.latLngToLayerPoint(mapObj.getCenter()); // 中心を直交座標系に変換
+      const upperCenter = mapObj.layerPointToLatLng([point.x, point.y - 20]); // 中心の少し上を直交座標系で求めて緯度経度に戻す
+
+      // 地図の動きに合わせてピンを動かす
+      centerPin.setLatLng(upperCenter);
+      centerCross.setLatLng(mapObj.getCenter());
+    });
+    // 動かし終わったらピンを戻す
+    mapObj.on("moveend", function(e) {
+      centerPin.setLatLng(mapObj.getCenter());
+    });
   }
 
   onClickPrev() {
