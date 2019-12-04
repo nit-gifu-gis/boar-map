6c8a4146a2dcadce9364db3829eb3500cc4c26e2
lalaso2000 <raraso2000@icloud.com>
2019-12-04 23:35:08 +0900

    情報登録ページ作成 #13

---
+++ components/templates/AddBoar/index.jsx
@@ -0,0 +1,14 @@
+import "./addBoar.scss";
+
+import Header from "../../organisms/header";
+import Footer from "../../organisms/footer";
+
+const AddBoar = () => (
+  <div>
+    <Header />
+    工事中！
+    <Footer />
+  </div>
+);
+
+export default AddBoar;
---
+++ pages/add/boar/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+import Router from "next/router";
+import SessionManager from "../../../utils/session";
+import AddBoar from "../../../components/templates/AddBoar";
+
+class AddBoarPage extends React.Component {
+  static async getInitialProps(ctx) {
+    if (!SessionManager.isLogin(ctx)) {
+      if (ctx.res) {
+        ctx.res.writeHead(302, {
+          Location: "/login"
+        });
+        ctx.res.end();
+      } else {
+        Router.push("/login");
+      }
+    }
+    return { isLogin: true };
+  }
+
+  render() {
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>いのししマップぎふ - 詳細情報登録</title>
+        </Head>
+        <AddBoar />
+      </>
+    );
+  }
+}
+
+export default AddBoarPage;
