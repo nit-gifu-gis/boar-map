f75aad6fbec85a74af747bccab1e19133243a777
lalaso2000 <raraso2000@icloud.com>
2019-12-06 01:05:11 +0900

    入力フォームを作成 #13

--- components/organisms/boarForm/index.jsx
+++ components/organisms/boarForm/index.jsx
@@ -3,7 +3,34 @@ import Router from "next/router";
 import React from "react";
 import AddInfoFooter from "../../molecules/addInfoFooter";
 
+const TrapSelector = () => (
+  <div className="trap_selector">
+    <label>わなの種類</label>
+    <select name="trap" id="trap">
+      <option value="箱わな">箱わな</option>
+      <option value="くくりわな">くくりわな</option>
+      <option value="その他">その他</option>
+    </select>
+  </div>
+);
+
+const EnvSelector = () => (
+  <div className="env_selector">
+    <label>発見場所</label>
+    <select name="env" id="env">
+      <option value="山際">山際</option>
+      <option value="山地">山地</option>
+      <option value="その他">その他</option>
+    </select>
+  </div>
+);
+
 class BoarForm extends React.Component {
+  state = {
+    todayStr: "",
+    trapOrEnvSelector: <TrapSelector />
+  };
+
   constructor() {
     super();
     // ユーザーデータ取得(cookieから持ってくる)
@@ -22,6 +49,7 @@ class BoarForm extends React.Component {
     } else {
       return;
     }
+    this.state.todayStr = this.getTodayStr();
   }
 
   // 前へボタンを押したときの処理
@@ -33,6 +61,34 @@ class BoarForm extends React.Component {
   // 次へボタンを押したときの処理
   onClickNext() {
     window.alert("工事中！");
+  }
+
+  // 今日の日付を取得する
+  getTodayStr() {
+    const date = new Date();
+    const yyyy = date.getFullYear();
+    const mm = ("0" + (date.getMonth() + 1)).slice(-2);
+    const dd = ("0" + date.getDate()).slice(-2);
+    return yyyy + "-" + mm + "-" + dd;
+  }
+
+  // 区分が変更されたときに呼ばれる
+  onChangeDivision() {
+    const divisonSelect = document.forms.form.division;
+    const division = divisonSelect.options[divisonSelect.selectedIndex].value;
+    switch (division) {
+      case "死亡":
+        this.setState(_ => {
+          return { trapOrEnvSelector: <EnvSelector /> };
+        });
+        // this.state.trapOrEnvSelector = <EnvSelector />;
+        break;
+      default:
+        this.setState(_ => {
+          return { trapOrEnvSelector: <TrapSelector /> };
+        });
+        break;
+    }
   }
 
   render() {
@@ -48,10 +104,43 @@ class BoarForm extends React.Component {
           <p>
             位置情報確認：({Router.query.lat}, {Router.query.lng})
           </p>
-          <p>工事中！</p>
-          <p>工事中！！</p>
-          <p>工事中！！！</p>
-          <p>工事中！！！！</p>
+          <form name="form">
+            <div className="__division">
+              <label>区分</label>
+              <select
+                name="division"
+                id="division"
+                onChange={this.onChangeDivision.bind(this)}
+              >
+                <option value="調査捕獲">調査捕獲</option>
+                <option value="有害捕獲">有害捕獲</option>
+                <option value="死亡">死亡</option>
+              </select>
+            </div>
+            <div className="__date">
+              <label>捕獲年月日</label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                value={this.state.todayStr}
+              />
+            </div>
+            <div className="__trap_or_env">{this.state.trapOrEnvSelector}</div>
+            <div className="__sex">
+              <label>性別</label>
+              <select name="sex" id="sex">
+                <option value="オス">オス</option>
+                <option value="メス">メス</option>
+                <option value="不明">不明</option>
+              </select>
+            </div>
+            <div className="__length">
+              <label>体長(cm)</label>
+              <input type="number" step="0.01"></input>
+            </div>
+            {/* 体重は体長から計算して送信する（表示しない） */}
+          </form>
         </div>
         <AddInfoFooter
           prevBind={this.onClickPrev}
