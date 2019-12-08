728808036c967922912a7304cd9269a6707f69b3
lalaso2000 <raraso2000@icloud.com>
2019-12-09 00:28:48 +0900

    ワクチン追加実装 #20

---
+++ components/templates/ConfirmVaccine/index.jsx
@@ -0,0 +1,15 @@
+import "./confrimVaccine.scss";
+
+import Header from "../../organisms/header";
+import Footer from "../../organisms/footer";
+import VaccineConfirmForm from "../../organisms/vaccineConfirmForm";
+
+const ConfirmVaccine = () => (
+  <div>
+    <Header />
+    <VaccineConfirmForm />
+    <Footer />
+  </div>
+);
+
+export default ConfirmVaccine;
