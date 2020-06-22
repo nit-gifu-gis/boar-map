295d2e10bc6dab6c91b4aad310c4188b245d92b4
lalaso2000 <raraso2000@icloud.com>
2020-06-22 23:03:12 +0900

    dateInputにエラー時の見た目を設定 #100

--- components/atomos/dateInput/index.jsx
+++ components/atomos/dateInput/index.jsx
@@ -3,7 +3,16 @@ import "./deteInput.scss";
 import "../../../public/static/css/global.scss";
 import TextInput from "../textInput";
 
+// 初期値を設定しないと自動で今日の日付を初期値にします
+
 class DateInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isErrorForSafari: false
+    };
+  }
+
   isSafari() {
     const userAgent = window.navigator.userAgent.toLowerCase();
 
@@ -48,31 +57,43 @@ class DateInput extends React.Component {
     }
   }
 
-  setDate() {
+  setDateForSafari() {
     const yearValue = document.getElementById(this.props.id + "Year").value;
     const monthValue = document.getElementById(this.props.id + "Month").value;
     const dayValue = document.getElementById(this.props.id + "Day").value;
+    // 空文字がある場合はエラー
+    // 年が4桁じゃ無いのもエラー
+    if (yearValue.length != 4 || monthValue == "" || dayValue == "") {
+      // 未入力欄がある場合
+      document.getElementById(this.props.id).value = null;
+      console.log("未入力がある");
+      this.setState({
+        isErrorForSafari: true
+      });
+      return;
+    }
     const yyyy = ("0000" + yearValue).slice(-4);
     const mm = ("00" + monthValue).slice(-2);
     const dd = ("00" + dayValue).slice(-2);
     const date = yyyy + "-" + mm + "-" + dd;
     // window.alert(date);
-    if (yyyy != "" && mm != "" && dd != "") {
-      // 全部空文字じゃ無いなら日付として正しいか判定
-      const dt = new Date(yyyy, mm - 1, dd);
-      console.log(dt);
-      if (
-        dt.getFullYear() == yyyy &&
-        dt.getMonth() == mm - 1 &&
-        dt.getDate() == dd
-      ) {
-        document.getElementById(this.props.id).value = date;
-      } else {
-        document.getElementById(this.props.id).value = null;
-      }
+    // 全部空文字じゃ無いなら日付として正しいか判定
+    const dt = new Date(yyyy, mm - 1, dd);
+    console.log(dt);
+    if (
+      dt.getFullYear() == yyyy &&
+      dt.getMonth() == mm - 1 &&
+      dt.getDate() == dd
+    ) {
+      document.getElementById(this.props.id).value = date;
+      this.setState({
+        isErrorForSafari: false
+      });
     } else {
-      // 未入力欄がある場合
       document.getElementById(this.props.id).value = null;
+      this.setState({
+        isErrorForSafari: true
+      });
     }
   }
 
@@ -85,11 +106,15 @@ class DateInput extends React.Component {
   }
 
   render() {
+    let className;
     if (this.state.isBrowser) {
+      // 初期値設定
       let DateY = "";
       let DateM = "";
       let DateD = "";
       if (this.props.date != null) {
+        // 外部から初期値が指定されている場合
+        // "-"区切りと"/"区切りに対応
         let date = this.props.date.split(" ")[0].split("-");
         if (date.length == 3) {
           DateY = date[0];
@@ -104,6 +129,7 @@ class DateInput extends React.Component {
           }
         }
       } else {
+        // 初期値が設定されていない場合は今日の日付をセット
         const today = new Date();
         DateY = ("0000" + today.getFullYear()).slice(-4);
         DateM = ("00" + (today.getMonth() + 1)).slice(-2);
@@ -121,7 +147,8 @@ class DateInput extends React.Component {
                   id={this.props.id + "Year"}
                   placeholder="西暦"
                   defaultValue={DateY}
-                  onChange={this.setDate.bind(this)}
+                  onChange={this.setDateForSafari.bind(this)}
+                  error={this.state.isErrorForSafari || this.props.error}
                 />
               </div>
               年
@@ -135,7 +162,8 @@ class DateInput extends React.Component {
                   step="1"
                   id={this.props.id + "Month"}
                   defaultValue={DateM}
-                  onChange={this.setDate.bind(this)}
+                  onChange={this.setDateForSafari.bind(this)}
+                  error={this.state.isErrorForSafari || this.props.error}
                 />
               </div>
               月
@@ -149,7 +177,8 @@ class DateInput extends React.Component {
                   step="1"
                   id={this.props.id + "Day"}
                   defaultValue={DateD}
-                  onChange={this.setDate.bind(this)}
+                  onChange={this.setDateForSafari.bind(this)}
+                  error={this.state.isErrorForSafari || this.props.error}
                 />
               </div>
               日
@@ -165,11 +194,15 @@ class DateInput extends React.Component {
           </div>
         );
       } else {
+        className = "date-input-div__input";
+        if (this.props.error) {
+          className += "--error";
+        }
         return (
           <div className="date-input-div">
             <input
               type="date"
-              className="date-input"
+              className={className}
               name={this.props.name}
               id={this.props.id}
               defaultValue={
