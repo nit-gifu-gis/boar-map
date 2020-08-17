d372a01bef105af41e4765781da308e7484228b4
lalaso2000 <raraso2000@icloud.com>
2020-08-17 16:34:01 +0900

    検索ボックス調整 #126

--- components/organisms/searchForm/index.jsx
+++ components/organisms/searchForm/index.jsx
@@ -9,9 +9,28 @@ import DateInput from "../../atomos/dateInput";
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      year: null,
+      date1: null,
+      date2: null,
+      city: null,
+      onClick: data => {
+        console.log(data);
+      }
+    };
+    if (props.onClick != undefined) {
+      this.state.onClick = props.onClick;
+    }
   }
 
   onClickSearchButton() {
+    const data = {
+      year: this.state.year,
+      date1: this.state.date1,
+      date2: this.state.date2,
+      city: this.state.city
+    };
+    this.state.onClick(data);
     alert("こーじちゅー");
   }
 
@@ -20,31 +39,31 @@ class SearchForm extends React.Component {
       <div className="search-form">
         <div className="search-form__title">検索条件</div>
         <div className="search-form__form">
-          <table className="search-form__form__table">
-            <tr>
-              <td>年度</td>
-              <td className="search-form__form__table__input">
-                <TextInput id="year" type="number" />
-              </td>
-            </tr>
-            <tr>
-              <td>日付</td>
-              <td className="search-form__form__table__input">
-                <DateInput id="date1" /> 〜 <DateInput id="date2" />
-              </td>
-            </tr>
-            <tr>
-              <td>市町村</td>
-              <td className="search-form__form__table__input">
-                <TextInput id="city" type="text" />
-              </td>
-            </tr>
-          </table>
-          <div className="search-form__form__button-div">
-            <div className="search-form__form__button-div__button">
-              <RoundButton bind={this.onClickSearchButton.bind(this)}>
-                検索
-              </RoundButton>
+          <div className="search-form__form__grid">
+            <div className="search-form__form__grid__title search-form__form__grid__date">
+              日付
+            </div>
+            <div className="search-form__form__grid__input search-form__form__grid__date">
+              <div className="search-form__form__grid__input__date">
+                <DateInput id="date1" />
+              </div>
+              <div className="search-form__form__grid__input__tilda">〜</div>
+              <div className="search-form__form__grid__input__date">
+                <DateInput id="date2" />
+              </div>
+            </div>
+            <div className="search-form__form__grid__title search-form__form__grid__city">
+              市町村
+            </div>
+            <div className="search-form__form__grid__input search-form__form__grid__city">
+              <TextInput id="city" />
+            </div>
+            <div className="search-form__form__grid__button-div">
+              <div className="search-form__form__grid__button-div__button">
+                <RoundButton bind={this.onClickSearchButton.bind(this)}>
+                  検索
+                </RoundButton>
+              </div>
             </div>
           </div>
         </div>
