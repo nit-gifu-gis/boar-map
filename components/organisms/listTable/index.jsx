441101a2cf6328bf1fc1b7098c9f377e5a9ae597
lalaso2000 <raraso2000@icloud.com>
2020-09-04 17:02:37 +0900

    表の文字部分を整形 #126

--- components/organisms/listTable/index.jsx
+++ components/organisms/listTable/index.jsx
@@ -15,22 +15,30 @@ class ListTable extends React.Component {
     const features = this.props.features;
     const featuresList = features.map(f => {
       const data = f.properties;
+      // メッシュ番号から市町村を取り出す
+      const cityPattern = /(^\D+)\d-?\d/;
+      const cityResult = cityPattern.exec(data["メッシュ番号"]);
+      const city = cityResult != null ? cityResult[1] : "";
+      // 捕獲年月日からは時刻を削除
+      const datePattern = /(^[\d/-]+)\s.*/;
+      const dateResult = datePattern.exec(data["捕獲年月日"]);
+      const date = dateResult != null ? dateResult[1] : "";
       return (
         <tr className="list-table__table__row" key={data["ID$"]}>
-          <td>{data["ID$"]}</td>
-          <td>{data["入力者"]}</td>
-          <td>{data["メッシュ番号"]}</td>
-          <td>{data["区分"]}</td>
-          <td>{data["捕獲年月日"]}</td>
-          <td>{data["罠・発見場所"]}</td>
-          <td>{data["捕獲頭数"]}</td>
-          <td>{data["幼獣・成獣"]}</td>
-          <td>{data["性別"]}</td>
-          <td>{data["妊娠の状況"]}</td>
-          <td>{data["体長"]}</td>
-          <td>{data["処分方法"]}</td>
-          <td>{data["備考"]}</td>
-          <td>{data["画像ID"]}</td>
+          <td style={{ textAlign: "right" }}>{data["ID$"]}</td>
+          <td style={{ textAlign: "left" }}>{data["入力者"]}</td>
+          <td style={{ textAlign: "left" }}>{city}</td>
+          <td style={{ textAlign: "left" }}>{data["区分"]}</td>
+          <td style={{ textAlign: "left" }}>{date}</td>
+          <td style={{ textAlign: "left" }}>{data["罠・発見場所"]}</td>
+          <td style={{ textAlign: "right" }}>{data["捕獲頭数"]}</td>
+          <td style={{ textAlign: "left" }}>{data["幼獣・成獣"]}</td>
+          <td style={{ textAlign: "left" }}>{data["性別"]}</td>
+          <td style={{ textAlign: "left" }}>{data["妊娠の状況"]}</td>
+          <td style={{ textAlign: "right" }}>{data["体長"]}</td>
+          <td style={{ textAlign: "left" }}>{data["処分方法"]}</td>
+          <td style={{ textAlign: "left" }}>{data["備考"]}</td>
+          <td style={{ textAlign: "left" }}>{data["画像ID"]}</td>
         </tr>
       );
     });
@@ -45,14 +53,24 @@ class ListTable extends React.Component {
               <th>市町村</th>
               <th>区分</th>
               <th>捕獲年月日</th>
-              <th>わなの種類・発見場所</th>
+              <th>
+                わなの種類
+                <br />
+                発見場所
+              </th>
               <th>捕獲頭数</th>
               <th>幼獣・成獣の別</th>
               <th>性別</th>
               <th>妊娠の状況</th>
               <th>体長</th>
               <th>処分方法</th>
-              <th>備考（捕獲を手伝った者の氏名）（遠沈管番号）</th>
+              <th>
+                備考
+                <br />
+                （捕獲を手伝った者の氏名）
+                <br />
+                （遠沈管番号）
+              </th>
               <th>写真</th>
             </tr>
             {featuresList}
