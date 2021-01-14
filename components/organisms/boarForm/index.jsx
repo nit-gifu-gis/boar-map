94da640d91cc828c0d9f9379aa4c6d567a2bfb59
lalaso2000 <raraso2000@icloud.com>
2021-01-14 10:44:50 +0900

    区分「狩猟」「その他」を選択時に「わなの種類」を表示するように変更 #166

--- components/organisms/boarForm/index.jsx
+++ components/organisms/boarForm/index.jsx
@@ -67,8 +67,6 @@ class BoarForm extends React.Component {
       const division = detail["properties"]["区分"];
       switch (division) {
         case "死亡":
-        case "狩猟":
-        case "その他":
           this.setState(_ => {
             return {
               trapOrEnv: ENV,
@@ -266,8 +264,6 @@ class BoarForm extends React.Component {
     let trapOrEnv;
     switch (division) {
       case "死亡":
-      case "狩猟":
-      case "その他":
         trapOrEnv = form.env.options[form.env.selectedIndex].value;
         break;
       default:
@@ -341,8 +337,6 @@ class BoarForm extends React.Component {
     const division = divisonSelect.options[divisonSelect.selectedIndex].value;
     switch (division) {
       case "死亡":
-      case "狩猟":
-      case "その他":
         this.setState(_ => {
           return { trapOrEnv: ENV, isBox: false };
         });
