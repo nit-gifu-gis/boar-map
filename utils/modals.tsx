bb7b361eda7564e3434f1b91188057fe5163b770
lalaso2000 <raraso2000@icloud.com>
2021-03-17 21:44:03 +0900

    モーダルが開いている間にスクロールできるバグを修正

--- utils/modals.tsx
+++ utils/modals.tsx
@@ -3,23 +3,51 @@ import AlertModal from '../components/molecules/alertModal';
 import ConfirmModal from '../components/molecules/confirmModal';
 
 
-export const alert = (message: string) => {
+export const alert = async (message: string) => {
+  return await showModal('alert', message);
+}
+
+
+export const confirm = async (message: string) => {
+  return await showModal('confirm', message);
+}
+
+// confirmとalertはほぼ同じ動作するので動作をまとめる
+const showModal = (type: "confirm" | "alert", message: string) => {
   // nextのアプリケーションが描画される要素を取得
   const nextDiv = document.getElementById("__next");
+  // スクロール禁止（結構無理やり止めてる）
+  const x = window.pageXOffset;
+  const y = window.pageYOffset;
+  const handleScroll = () => window.scrollTo(x, y);
+  window.addEventListener('scroll', handleScroll);
   // モーダルを表示するdivを作成
   const wrapper = nextDiv.appendChild(document.createElement("div"));
+  wrapper.style.width = '100vw';
+  wrapper.style.height = '100vh';
+  wrapper.style.position = 'absolute';
+  wrapper.style.top = y + 'px';
+  wrapper.style.left = x + 'px';
   // モーダルを消去する関数
   const cleanup = () => {
     ReactDOM.unmountComponentAtNode(wrapper);
+    window.removeEventListener('scroll', handleScroll);
     return setTimeout(() => wrapper.remove());
   };
   const promise = new Promise<Boolean>((resolve, reject) => {
     try {
+      const modal =
+        type === 'confirm' ?
+          <ConfirmModal
+            message={message}
+            resolve={resolve}
+            cleanup={cleanup} /> :
+          <AlertModal
+            message={message}
+            resolve={resolve}
+            cleanup={cleanup} />;
       ReactDOM.render(
-        <AlertModal
-          message={message}
-          resolve={resolve}
-          cleanup={cleanup} />,
+        modal,
         wrapper
       );
     } catch (e) {
@@ -29,33 +57,4 @@ export const alert = (message: string) => {
     }
   });
   return promise;
-}
-
-
-export const confirm = (message: string) => {
-  // nextのアプリケーションが描画される要素を取得
-  const nextDiv = document.getElementById("__next");
-  // モーダルを表示するdivを作成
-  const wrapper = nextDiv.appendChild(document.createElement("div"));
-  // モーダルを消去する関数
-  const cleanup = () => {
-    ReactDOM.unmountComponentAtNode(wrapper);
-    return setTimeout(() => wrapper.remove());
-  };
-  const promise = new Promise<Boolean>((resolve, reject) => {
-    try {
-      ReactDOM.render(
-        <ConfirmModal
-          message={message}
-          resolve={resolve}
-          cleanup={cleanup} />,
-        wrapper
-      );
-    } catch (e) {
-      cleanup();
-      reject(e);
-      throw e;
-    }
-  });
-  return promise;
-}
+};
