b5b42d9aec5ab92277ee13377b7cabe679ef7b6f
Junki Tomatsu <me@junki-t.net>
2022-03-11 16:01:39 +0900

    OGP/ロード画面

--- src/components/templates/loadingTemplate/index.tsx
+++ src/components/templates/loadingTemplate/index.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+
 const LoadingTemplate: React.FunctionComponent = () => {
   return (
     <div className='flex h-screen min-h-screen items-center justify-center overflow-auto'>
-      認証情報ロード中... (ここに認証情報ロード時の画面を作成する。)
+      <div className='text-center'>
+        <Image src="/login.png" alt="main logo" className="container__image" width='250' height='71' />
+        <h1>いのししマップぎふ</h1>
+        <div><span>岐阜県家畜対策公式Webアプリ</span></div>
+        <Image src='/spinner.gif' alt='Loading Icon' width='100' height='100'></Image>
+        <span className='block text-xl'>ロード中...</span>
+      </div>
     </div>
   );
 };
