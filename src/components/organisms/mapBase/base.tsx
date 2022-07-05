c93edef03ceb7023dfc27a2f549a09e7a9f370f2
Junki Tomatsu <me@junki-t.net>
2022-07-05 09:29:24 +0900

    V2.1.1 (#213)
    * 豚熱陽性高率エリアの表示設定のグローバル化
    
    * PCR結果インポートの表示ユーザーの調整
    
    * ファイルアップロード時の容量エラーについて表示するように実装
    
    * レイヤー名の変更
    
    * 豚熱陽性高率エリアの期間外の削除
    
    * 位置情報周りの修正
    [meta]
    [version]Version 2.1.1α[/version]
    [contents]
    豚熱陽性高率エリアの表示方法を変更しました。
    データインポートに関連するバグを修正しました。
    位置情報の取得方法を変更しました。
    [/contents]
    [/meta]

--- src/components/organisms/mapBase/base.tsx
+++ src/components/organisms/mapBase/base.tsx
@@ -31,7 +31,7 @@ import RoundButton from '../../atomos/roundButton';
 
 const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
   const router = useRouter();
-  const [loc, setLoc] = useState<Location | null>(null);
+  const [, setLoc] = useState<Location | null>(null);
   const [watchPosId, setWatchPosId] = useState<number | null>(null);
   const [overlayList, setOverlayList] = useState<Record<string, L.MarkerClusterGroup | L.LayerGroup>>({});
   const { currentUser } = useCurrentUser();
@@ -46,8 +46,8 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
   const [labelVisible, setLabelVisible] = useState(false);
   const [searchButtonLabel, setSearchButtonLabel] = useState("検索");
 
-  // const [myLocMarker, setMyLocMarker] = useState<L.Marker | null>(null);
-  let myLocMarker: L.Marker | null = null;
+  const [, setMyLocMarker] = useState<L.Marker | null>(null);
+  // let myLocMarker: L.Marker | null = null;
 
   // クラスタ設定
   const clusterIconCreate = (
@@ -69,13 +69,6 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
   };
 
   const setupMap = async () => {
-    if (loc == null) {
-      setLoc({
-        lat: 35.39135,
-        lng: 136.722418,
-      });
-    }
-  
     const overlay: { [key: string]: L.MarkerClusterGroup | L.LayerGroup } = {};
     if (Object.keys(overlay).length === 0 && currentUser != null) {
       // レイヤーの追加
@@ -131,7 +124,7 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
         const lg =  L.layerGroup([mcg]);
         setButanetsuLayerID(lg.getLayerId(mcg));
 
-        overlay['豚熱陽性確認地点'] = lg;
+        overlay['豚熱陽性高率エリア'] = lg;
       }
   
       if (hasReadPermission('report', currentUser)) {
@@ -180,13 +173,13 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
             feature.geometry.coordinates[0].forEach(l => {
               coordinates.push([l[1], l[0]]);
             });
-            polygons.push(L.polygon(coordinates, {
+            const p = L.polygon(coordinates, {
               color: '#0288d1',
               weight: 2,
-              fill: true,
-              fillColor: '#0288d1',
-              opacity: 0.6 
-            }));
+              fill: false 
+            });
+
+            polygons.push(p);
           });
         });    
         overlay['ワクチンメッシュ'] = L.layerGroup(polygons);
@@ -318,9 +311,19 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
       const lat = pos.coords.latitude;
       const lng = pos.coords.longitude;
 
-      if (loc == null) return;
-      loc.lat = lat;
-      loc.lng = lng;
+      setLoc(loc => {
+        if(loc == null) {
+          return {
+            lat: lat,
+            lng: lng
+          };
+        }
+
+        loc.lat = lat;
+        loc.lng = lng;
+
+        return loc;
+      });
       setCurrentLocation(true);
     };
 
@@ -415,18 +418,30 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
           );
 
           // 描画するマーカーの生成
-          const newMarkers = newFeatures.map((f) => makeMarker(f, key as layerType));
-          if(key === "豚熱陽性確認地点") {
-            setButanetsuLayerID(id => {
-              const l = overlayList[key] as L.LayerGroup;
-              const lg = overlayList[key].getLayer(id) as L.MarkerClusterGroup;
-              lg.addLayers(newMarkers);
-              makeCircleMarkers(newFeatures as ButanetsuFeature[]).forEach(m => {
-                l.addLayer(m);
+          if(key === "豚熱陽性高率エリア") {
+            const asyncTask = async () => {
+              const res = await fetch(SERVER_URI + '/Settings/Butanetsu', {
+                headers: {
+                  'X-Access-Token': getAccessToken()
+                }
               });
-              return id;
-            });
+              const settings = await res.json();
+
+              const circleMarkers = await makeCircleMarkers(settings, newFeatures as ButanetsuFeature[]);
+              const newMarkers = (await filterButanetsu(settings, newFeatures as ButanetsuFeature[])).map(f => makeMarker(f, key as layerType));
+              setButanetsuLayerID(id => {
+                const l = overlayList[key] as L.LayerGroup;
+                const lg = overlayList[key].getLayer(id) as L.MarkerClusterGroup;
+                lg.addLayers(newMarkers);
+                circleMarkers.forEach(m => {
+                  l.addLayer(m);
+                });
+                return id;
+              });
+            };
+            asyncTask();
           } else {
+            const newMarkers = newFeatures.map((f) => makeMarker(f, key as layerType));
             (overlayList[key] as L.MarkerClusterGroup).addLayers(newMarkers);
           }
         }
@@ -437,13 +452,16 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
     setLoading(false);
   };
 
-  const makeCircleMarkers = (features: ButanetsuFeature[]): L.Circle[] => {
-    const cookies = parseCookies();
-    const settings = cookies['butanetsu'] != null ? JSON.parse(cookies['butanetsu']) : {
-      area: 10,
-      month: 5,
-    };
+  const filterButanetsu = async (settings: Record<string, number>, features: ButanetsuFeature[]): Promise<ButanetsuFeature[]> => {
+    const show_date = new Date();
+    show_date.setHours(0);
+    show_date.setMinutes(0);
+    show_date.setSeconds(0);
+    show_date.setMonth(show_date.getMonth() - settings.month);
+    return features.filter(f=>show_date <= new Date(f.properties.捕獲年月日));
+  };
 
+  const makeCircleMarkers = async (settings: Record<string, number>, features: ButanetsuFeature[]): Promise<L.Circle[]> => {
     const show_date = new Date();
     show_date.setHours(0);
     show_date.setMinutes(0);
@@ -456,7 +474,7 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
       if(show_date <= date) {
         const loc = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]] as LatLngExpression;
         const markers = L.circle(loc, {
-          radius: settings.area * 1000,
+          radius: settings.radius * 1000,
           color: "#e33b3b",
           weight: 2,
           fill: true,
@@ -532,7 +550,7 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
         dataValue = f4.properties.作業開始時;
         break;
       }
-      case '豚熱陽性確認地点': {
+      case '豚熱陽性高率エリア': {
         const f5 = f as ButanetsuFeature;
         icon = markerIcon(butanetsuIconLink, formatDate(f5.properties.捕獲年月日));
         dataLabel = '捕獲年月日';
@@ -582,27 +600,34 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
 
   const setCurrentLocation = (moveMarker: boolean) => {
     if (myMap == null) return;
-    if (!loc?.lat || !loc.lng) {
+    setLoc(loc => {
+      if (!loc?.lat || !loc.lng) {
+        if (moveMarker) {
+          alert('位置情報の取得ができません。');
+        }
+        return loc;
+      }
+  
+      setMyLocMarker(myLocMarker => {
+        if (myLocMarker == null) {
+          myLocMarker = L.marker([loc.lat, loc.lng], { icon: myLocIcon });
+          try {
+            myLocMarker.addTo(myMap);
+          } catch {
+            /** */
+          }
+        }
+    
+        myLocMarker.setLatLng([loc.lat, loc.lng]);
+  
+        return myLocMarker;
+      });
       if (moveMarker) {
-        alert('位置情報の取得ができません。');
+        myMap.setView([loc.lat, loc.lng], 17);
       }
-      return;
-    }
 
-    if (myLocMarker == null) {
-      myLocMarker = L.marker([loc.lat, loc.lng], { icon: myLocIcon });
-      if (myLocMarker == null) return;
-      try {
-        myLocMarker.addTo(myMap);
-      } catch {
-        /** */
-      }
-    }
-
-    myLocMarker.setLatLng([loc.lat, loc.lng]);
-    if (moveMarker) {
-      myMap.setView([loc.lat, loc.lng], 17);
-    }
+      return loc;
+    });
   };
 
   const startWatchLocation = () => {
@@ -615,9 +640,19 @@ const MapBase_: React.FunctionComponent<MapBaseProps> = (props) => {
       const lat = pos.coords.latitude;
       const lng = pos.coords.longitude;
       // 表示は行わず，マーカーの移動のみ処理する
-      if (loc == null) return;
-      loc.lat = lat;
-      loc.lng = lng;
+      setLoc(loc => {
+        if(loc == null) {
+          return {
+            lat: lat,
+            lng: lng
+          };
+        }
+
+        loc.lat = lat;
+        loc.lng = lng;
+
+        return loc;
+      });
       setCurrentLocation(false);
     };
 
